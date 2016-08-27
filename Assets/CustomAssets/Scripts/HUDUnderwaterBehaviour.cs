4023c87c26bf4ea56a083e05ae851f48e7aba353
Kyle Zhou <kylezhou2002@hotmail.com>
2016-08-27 19:02:12 -0400

    Created HUD for game

---
+++ Assets/CustomAssets/Scripts/HUDUnderwaterBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUDUnderwaterBehaviour : MonoBehaviour {
+    [SerializeField] Image m_UnderwaterOverlay;
+    Color m_UnderwaterColor;
+    Color m_TransparentColor;
+
+	// Use this for initialization
+	void Start () {
+        m_UnderwaterColor = new Color(0, 0, 1.0f, 0.4f);
+        m_TransparentColor = new Color(0, 0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GlobalVars.isInWater)
+            // i'm in water
+            m_UnderwaterOverlay.color = m_UnderwaterColor;
+        else
+            m_UnderwaterOverlay.color = m_TransparentColor;
+	}
+}
--- Assets/WaterToggleBehaviour.cs
+++ Assets/WaterToggleBehaviour.cs
@@ -6,12 +6,19 @@ using UnityStandardAssets._2D;
 
 public class WaterToggleBehaviour : MonoBehaviour {
     [SerializeField] float m_MaxOxygen = 100.0f;
-    [SerializeField] Text m_OxyDisplay;
+    [SerializeField] float m_RegenRate = 0.5f;
+    [SerializeField] Slider m_OxyValueDisplay;
+    [SerializeField] Image m_OxyBarFill;
+    [SerializeField] Text m_OxyDebugValue;
+    [SerializeField] Image m_UnderwaterDisplay;
     bool m_HasOxygen = false;
     float m_CurrentOxygen;
-    Color m_DefaultColour = new Color(0x31 / 255.0f, 0x4D / 255.0f, 0x79 / 255.0f);
     Camera m_Camera;
     Rigidbody2D m_Rigid2D;
+
+    // Colour Declarations
+    Color m_DrainingBlue = new Color(0.435f, 0.125f, 0.667f);
+    Color m_DefaultBGColour = new Color(0x31 / 255.0f, 0x4D / 255.0f, 0x79 / 255.0f);
 
     // Use this for initialization
     void Start () {
@@ -23,24 +30,35 @@ public class WaterToggleBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         GlobalVars.isInWater = CrossPlatformInputManager.GetButton("Water");
+        if (m_OxyBarFill)
+        {
+            if (m_HasOxygen)
+                m_OxyBarFill.color = Color.blue;
+            else
+                m_OxyBarFill.color = Color.red;
+        }
         if (GlobalVars.isInWater && checkOxygen())
         {
+            if(m_OxyBarFill)
+                m_OxyBarFill.color = m_DrainingBlue;
             m_CurrentOxygen -= 1.0f * Time.deltaTime;
             m_Rigid2D.drag = 10;
-            m_Camera.backgroundColor = Color.blue;
+            //m_Camera.backgroundColor = Color.blue;
         }
         else
         {
             GlobalVars.isInWater = false;
             if (m_CurrentOxygen < m_MaxOxygen)
-                m_CurrentOxygen += 0.5f * Time.deltaTime;
+                m_CurrentOxygen += m_RegenRate * Time.deltaTime;
             if(m_CurrentOxygen > 0.5 * m_MaxOxygen)
                 m_HasOxygen = true;
-            m_Camera.backgroundColor = m_DefaultColour;
+            //m_Camera.backgroundColor = m_DefaultBGColour;
             m_Rigid2D.drag = 0;
         }
-
-        m_OxyDisplay.text = m_CurrentOxygen.ToString();
+        if(m_OxyDebugValue != null)
+            m_OxyDebugValue.text = m_CurrentOxygen.ToString();
+        if (m_OxyValueDisplay != null)
+            m_OxyValueDisplay.value = m_CurrentOxygen / m_MaxOxygen;
     }
 
     bool checkOxygen()
