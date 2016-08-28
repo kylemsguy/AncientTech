14dc2fb16c81a019abd0ec460df14024671ad6de
Kyle Zhou <kylezhou2002@hotmail.com>
2016-08-28 13:35:20 -0400

    Default death sound set, fixed death timeout

--- Assets/Standard Assets/2D/Scripts/Restarter.cs
+++ Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -9,6 +9,20 @@ namespace UnityStandardAssets._2D
     {
         [SerializeField] private AudioSource m_BGMSource;
         [SerializeField] private AudioClip m_DeathClip;
+        private AudioSource[] m_AllAudioSources;
+
+        private void Start()
+        {
+            if (!m_BGMSource)
+                m_BGMSource = FindObjectOfType<AudioSource>();
+            if (!m_DeathClip)
+                m_DeathClip = Resources.Load<AudioClip>("Sounds/explosion1");
+        }
+
+        private void Awake()
+        {
+            m_AllAudioSources = FindObjectsOfType<AudioSource>();
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
@@ -20,18 +34,26 @@ namespace UnityStandardAssets._2D
                 }
                 else
                 {
+                    StopAllAudio();
                     m_BGMSource.PlayOneShot(m_DeathClip);
                     // stop time
-                    //Time.timeScale = 0;
-                    StartCoroutine("waitDeath");
+                    Time.timeScale = 0;
+                    StartCoroutine("WaitForDeath");
                 }
             }
         }
 
-        private IEnumerator waitDeath()
+        private IEnumerator WaitForDeath()
         {
-            yield return new WaitForSeconds(1.0f);
+            yield return StartCoroutine(CoroutineUtilities.WaitForRealTime(1));
             SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+            Time.timeScale = 1;
+        }
+
+        private void StopAllAudio()
+        {
+            foreach(var source in m_AllAudioSources)
+                source.Stop();
         }
     }
 }
