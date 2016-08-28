0964370fe64765f42c3ac7e2e58cee3f6ce03f8a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-08-28 13:50:20 -0400

    Add slingshot

---
+++ Assets/CustomAssets/Scripts/ShootProjectileBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootProjectileBehaviour : MonoBehaviour {
+
+    public GameObject m_ProjectileObject;
+    public Vector2 m_InitialVelocity;
+    public Vector2 m_InitialPositionOffset;
+    public void Shoot() {
+        var newObj = Instantiate<GameObject>(m_ProjectileObject);
+        newObj.transform.position = transform.position + new Vector3(m_InitialPositionOffset.x, m_InitialPositionOffset.y);
+        newObj.GetComponent<Rigidbody2D>().velocity = m_InitialVelocity;
+    }
+}
---
+++ Assets/CustomAssets/Scripts/SlingBallBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlingBallBehaviour : MonoBehaviour {
+    public GameObject m_DeathParticleSystem;
+
+    void Update() {
+        GetComponent<Rigidbody2D>().velocity = GlobalVars.isInWater ? new Vector2(-2, 0) : new Vector2(-4, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        var particles = Instantiate(m_DeathParticleSystem, transform.position, Quaternion.identity);
+        Destroy(particles, 1);
+        Destroy(gameObject);
+    }
+}
